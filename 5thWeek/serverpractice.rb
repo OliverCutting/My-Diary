14342c532dfd64629ec0afbb2120892b7d82b8ab
Oliver Cutting <oliver.cutting@gmail.com>
2022-02-02 11:57:36 +0000

    updated

---
+++ 5thWeek/serverpractice.rb
@@ -0,0 +1,11 @@
+require 'socket'
+
+server = TCPServer.new(2345)
+socket = server.accept
+socket.puts "What do you say?"
+they_said = socket.gets.chomp
+until they_said == "quit"
+  socket.puts "You said: #{they_said}. Goodbye!"
+  they_said = socket.gets.chomp
+end
+socket.close
